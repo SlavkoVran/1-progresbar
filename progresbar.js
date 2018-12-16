954512b43b64e13df9f14b3892e1fb04bb390b96
Slavko Vranešević <44097277+SlavkoVran@users.noreply.github.com>
2018-12-16 19:19:35 +0100

    Update progresbar.js

--- progresbar.js
+++ progresbar.js
@@ -1,35 +1,32 @@
-  let elem = document.getElementById("bar");   
-    let width = 1;
-    let id = setInterval(frame, 1000);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-      } 
-      else {
-        width++; 
-        elem.style.width = width + '%'; 
-      }
-    }
+let width = 1;
+let elem = document.getElementById("bar");
 
-
-  function premotaj() {
-    width--; 
-    elem.style.width = width + '%';    
-    
-  
-  }
-
-  function zaustavi() {
+function pokreni() {
+  id = setInterval(frame, 1000);
+  function frame() {
+    if (width >= 100) {
       clearInterval(id);
+    }
+    else {
+      width++;
+      elem.style.width = width + '%';
+    }
   }
-    
 
+}
+function premotaj() {
+  clearInterval(id);
+  id = setInterval(frame1, 1000);
+  function frame1() {
+    width--;
+    elem.style.width = width + '%';
+  }
+}
 
+function zaustavi() {
+  clearInterval(id);
+}
 
-
-
-
-
-  document.getElementById("dugmePokreni").addEventListener("click", frame);
-  document.getElementById("dugmePremotaj").addEventListener("click", premotaj);
-  document.getElementById("dugmeZaustavi").addEventListener("click", zaustavi);
+document.getElementById("dugmePokreni").addEventListener("click", pokreni);
+document.getElementById("dugmePremotaj").addEventListener("click", premotaj);
+document.getElementById("dugmeZaustavi").addEventListener("click", zaustavi);
